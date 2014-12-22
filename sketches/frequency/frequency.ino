8f40c69597857f097694c6f1ac7f409f3a537a2d
ksheng- <ksheng7746@gmail.com>
2014-12-22 00:00:41 -0500

    test sketches

---
+++ sketches/frequency/frequency.ino
@@ -0,0 +1,50 @@
+const int pot = A0;
+const int bcdPins[] = {2,3,4,5};
+const unsigned char mask = 1;
+int counter;
+int prevVal;
+int currentVal;
+
+void setup(){
+  for(int i =0; i<sizeof(bcdPins)/sizeof(int);i++){
+    pinMode(bcdPins[i], OUTPUT);
+    prevVal =1;
+  }
+    Serial.begin(9600);
+}
+
+void loop(){
+int freq = getFreq();
+//convert freq to displayVal
+int displayVal = (freq/10);
+
+toDecoder(displayVal);
+
+}
+
+void toDecoder(unsigned char num){
+  for(int i =0; i<(sizeof(bcdPins)/sizeof(int)); i++){
+    digitalWrite(bcdPins[i], (num&mask));
+    num = num >> 1;
+   }
+}
+
+int getFreq(){
+counter =0;
+for(int i =0; i<100; i++){
+  int t1=millis();
+  int sensorVal = analogRead(pot);
+  if(sensorVal<300)
+    currentVal =0;
+  if(sensorVal>800)
+    currentVal = 1;
+  if(prevVal!=currentVal)
+    counter++;
+  prevVal=currentVal;
+  int t2=millis();
+  delay(5-(t2-t1));
+   Serial.println(sensorVal);
+}
+ ;
+  return (counter);
+}
---
+++ sketches/potential/potential.ino
@@ -0,0 +1,23 @@
+const int pot = A0;
+const int bcdPins[] = {2,3,4,5};
+const unsigned char mask = 1;
+
+void setup(){
+  for(int i =0; i<sizeof(bcdPins)/sizeof(int);i++){
+    pinMode(bcdPins[i], OUTPUT);
+  }
+}
+
+void loop(){
+int sensorVal = analogRead(pot);
+unsigned char displayVal = sensorVal/103;
+toDecoder(displayVal);
+delay(50);
+}
+
+void toDecoder(unsigned char num){
+  for(int i =0; i<(sizeof(bcdPins)/sizeof(int)); i++){
+    digitalWrite(bcdPins[i], (num&mask));
+    num = num >> 1;
+   }
+}
